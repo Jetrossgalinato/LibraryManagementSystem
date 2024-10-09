59fba279f9ca0db374170506564e8e39f9f75986
Jetrossgalinato <jetrossneri07@gmail.com>
2024-10-09 16:17:40 +0800

    feat: inputted icons in email and password

--- Library Management System/src/components/layout/AppLayout.vue
+++ Library Management System/src/components/layout/AppLayout.vue
@@ -30,9 +30,7 @@ function onClick() {
       </v-app-bar>
 
       <v-main>
-        <v-container>
-          <slot name="content"></slot>
-        </v-container>
+        <slot name="content"></slot>
       </v-main>
 
       <v-footer
